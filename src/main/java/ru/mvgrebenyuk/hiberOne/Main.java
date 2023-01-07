01b085c915f5fc02ff5b05de704e5842f090373b
Antonio <anikoanton@gmail.com>
2023-01-07 18:30:44 +0300

    11123

--- src/main/java/ru/mvgrebenyuk/hiberOne/Main.java
+++ src/main/java/ru/mvgrebenyuk/hiberOne/Main.java
@@ -25,7 +25,7 @@ public class Main {
             2. Для обеих сущностей создаете Dao классы. Работу с SessionFactory выносите во вспомогательный класс;
             3. * Создаете сервис, позволяющий по id покупателя узнать список купленных им товаров, и по id товара узнавать список покупателей этого товара;
            */
-            CostumerDao costumerDao= new CostumerDao(sessionFactoryUtils);
+            CostumerDao costumerDao= new CostumerDao(sessionFactoryUtils); //
 
 
             // * Создаете сервис, позволяющий по id покупателя узнать список купленных им товаров,
